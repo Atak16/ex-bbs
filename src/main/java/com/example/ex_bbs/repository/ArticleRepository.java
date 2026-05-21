cc5204ca8e626a9997553ba593b3a39a70c5c593
Akihide Takahashi <akihide.tak16@gmail.com>
2026-05-21 16:56:15 +0900

    5.記事とコメントの削除

--- src/main/java/com/example/ex_bbs/repository/ArticleRepository.java
+++ src/main/java/com/example/ex_bbs/repository/ArticleRepository.java
@@ -1,6 +1,8 @@
 package com.example.ex_bbs.repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
@@ -8,6 +10,7 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.ex_bbs.domain.Article;
 
@@ -53,5 +56,17 @@ public class ArticleRepository {
         SqlParameterSource param = new BeanPropertySqlParameterSource(article);
         String sql = "INSERT INTO articles (name, content) VALUES (:name, :content)";
         template.update(sql, param);
-    }   
+    }
+
+    /**
+     * 記事を削除
+     * 
+     * @param id 削除する記事ID
+     */
+    public void deleteById(Integer id) {
+        String sql = "DELETE FROM articles WHERE id = :id";
+        Map<String, Object> param = new HashMap<>();
+        param.put("id", id);
+        template.update(sql, param);
+    }
 }
